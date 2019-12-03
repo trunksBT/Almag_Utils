e39236a0de516c2259bd51ee60639ed53158fd64
pkorycin <pkorycin>
2019-12-03 12:30:24 +0100

    Add the printUtils

---
+++ PrintUtils.cpp
@@ -0,0 +1,46 @@
+#include "Utils/PrintUtils.hpp"
+#include <sstream>
+#include <Utils/Logger.hpp>
+
+namespace
+{
+constexpr const char* SPACE_AS_SEPARATOR = " ";
+}
+
+namespace printUtils
+{
+void printHex(const char* msg, Hex input)
+{
+   LOG(trace) << msg << std::hex << static_cast<int>(input);
+}
+
+void printFrame(const char* msg, const Hexes& input)
+{
+   std::stringstream stream;
+   stream << msg;
+   for (const auto& it : input)
+   {
+      stream << std::hex << static_cast<int>(it) << " ";
+   }
+   LOG(trace) << stream.str();
+}
+
+std::string toString(const Hexes& value)
+{
+   std::stringstream stream;
+   for (const auto& it : value)
+   {
+      stream << std::hex << static_cast<int>(it) << SPACE_AS_SEPARATOR;
+   }
+   return stream.str();
+}  /// TODO remove the last space
+
+void printStrings(const Strings& hexes)
+{
+   for (const auto& it : hexes)
+   {
+      LOG(trace) << it;
+   }
+}
+
+}
---
+++ PrintUtils.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Utils/TypeAliases.hpp>
+
+namespace printUtils
+{
+std::string toString(const Hexes& value);
+void printHex(const char* msg, Hex input);
+void printFrame(const char* msg, const Hexes& input);
+void printStrings(const Strings& hexes);
+}
